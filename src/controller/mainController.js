c0d2f830cf84ec0a9a5989beae8aa03bc49b14f3
yhuzued <yusufmait@gmail.com>
2023-06-05 10:58:03 +0700

    implement main controller

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,1 +1,44 @@
 import '../assets/style.css';
+import updateWeather from './domController';
+
+function requestUrl(location) {
+  const key = '94c1fa00c6a040f6a5672750230106';
+  const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`;
+  return url;
+}
+
+async function getWeather(location) {
+  try {
+    const url = requestUrl(location);
+    const response = await fetch(url, { mode: 'cors' });
+    const weather = await response.json();
+    return weather;
+  } catch (error) {
+    throw new Error('Can\'t fetch data');
+  }
+}
+
+async function processData() {
+  try {
+    const location = document.getElementById('input-form').value;
+
+    let weather;
+    if (location === '') {
+      weather = await getWeather('london'); // default city
+    } else {
+      weather = await getWeather(location);
+    }
+
+    updateWeather(weather);
+  } catch (error) {
+    throw new Error('Can\'t process data');
+  }
+}
+
+function submit(e) {
+  e.preventDefault();
+  processData();
+  e.target.reset();
+}
+
+document.addEventListener('submit', (e) => submit(e));
